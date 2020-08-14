59db1c98dfbbe1fa0a319320fab08a2cf3d35398
Edgar Jose Rojas Lopez <edgarjrojasl@gmail.com>
2020-08-13 22:26:24 -0400

    Updates

---
+++ precache-manifest.c932216a541a1100f66d6904c3728f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59db2cc0a9e6725051388263b6138bd2",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "eeb64c7d71042cbca6b0",
+    "url": "/crwn-clothing/static/css/main.0a948ca9.chunk.css"
+  },
+  {
+    "revision": "4d42b2e44c83c7f0d498",
+    "url": "/crwn-clothing/static/js/2.cbe4bbb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.cbe4bbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb64c7d71042cbca6b0",
+    "url": "/crwn-clothing/static/js/main.cfea4c72.chunk.js"
+  },
+  {
+    "revision": "8558d1d6d2663c53b558",
+    "url": "/crwn-clothing/static/js/runtime-main.742f8efe.js"
+  }
+]);
